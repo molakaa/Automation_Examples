044ec4de668e26c5eefcc713724774b7e6ede111
molak <molak@SAIAPARNA>
2025-01-15 17:04:50 -0600

    pushing page load

---
+++ src/Testing/PageLoadTimeout.java
@@ -0,0 +1,26 @@
+package Testing;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PageLoadTimeout {
+
+	public static void main(String[] args) {
+    System.setProperty("webdriver.chrome.driver", "./Resources/chromedriver.exe");;
+    ChromeDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
+	Instant StartTime = Instant.now();
+	System.out.println(StartTime.toString());
+	driver.get("https://www.royalmansour.com/en/");
+	Instant EndTime = Instant.now();
+	System.out.println(EndTime.toString());
+	System.out.println("PageLoad Time:"+Duration.between(StartTime, EndTime).toSeconds()+ "Seconds");
+	System.out.println(driver.getTitle());
+	driver.close();
+	
+	}
+	}
