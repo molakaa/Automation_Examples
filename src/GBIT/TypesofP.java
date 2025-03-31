8aab16848448c27446214d1f8f228973a8678cac
molak <molak@SAIAPARNA.lan1>
2025-03-31 14:03:03 -0500

    Changes

--- src/GBIT/TypesofP.java
+++ src/GBIT/TypesofP.java
@@ -34,7 +34,8 @@ public class TypesofP {
          WebElement netLeaseLink = wait.until(ExpectedConditions.presenceOfElementLocated(
          By.xpath("//a[@href='/services/property-types/alternatives/net-lease' and text()='Net Lease']")));
          netLeaseLink.click();
-         driver.close();        
+         driver.close();  
+         
 	}
 
 }
