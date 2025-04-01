fcf0bc39a06b9777cb26f1ae90c328f65fb8d941
molakaa <molaka.saiaparna96@gmail.com>
2025-03-31 22:17:44 -0500

    Commit 2

--- src/GBIT/Lease.java
+++ src/GBIT/Lease.java
@@ -18,28 +18,40 @@ public class Lease {
 		WebDriver driver = new ChromeDriver();
         driver.get("https://www.cbre.com");
         driver.manage().window().maximize();
-	     Thread.sleep(2000);
-	     driver.findElement(By.id("onetrust-accept-btn-handler")).click();
-	     String title = driver.getTitle();
-	     System.out.println("Page Title: " + title );
-	     String url = driver.getCurrentUrl();
-		 System.out.println("Page url" + url);
+	    Thread.sleep(2000);
+	    
+	    driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+	    String title = driver.getTitle();
+	    System.out.println("Page Title: " + title );
+	    String url = driver.getCurrentUrl();
+		System.out.println("Page url" + url);
+		
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement propertiesSpan = driver.findElement(By.xpath("//span[@class='cbre-c-globalNav__link' and contains(text(), 'Properties')]"));
         Actions actions = new Actions(driver);
         actions.moveToElement(propertiesSpan).perform();
-        WebElement dropdownMenu = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//div[@class='cbre-c-globalNavDropdown' and @aria-hidden='false']")));
+	    Thread.sleep(2000);
+        
+        WebElement D = wait.until(ExpectedConditions.visibilityOfElementLocated(
+        By.xpath("//div[@class='cbre-c-globalNavDropdown' and @aria-hidden='false']")));
         WebElement leaseMenu = driver.findElement(By.xpath("//a[@class='cbre-c-globalNavDropdown__link' and contains(text(), 'Properties for Lease')]"));
         leaseMenu.click();
         System.out.println("After clicking 'Lease Menu', the new page title is: " + driver.getTitle());      
         Thread.sleep(2000);
+        
         driver.findElement(By.id("city-search")).clear();
         Thread.sleep(2000);
+        
         driver.findElement(By.id("city-search")).sendKeys("Irving TX, USA");
         Thread.sleep(2000);
+        
         actions.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).perform();
         Thread.sleep(5000);
+        
+        WebElement W = driver.findElement(By.id("city-search"));
+        String S = W.getAttribute("value");
+        System.out.println("Entered text: " + S);
+        
         driver.quit();
 	    
 	}
---
+++ src/GBIT/REPMS.java
@@ -0,0 +1,35 @@
+package GBIT;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class REPMS {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./Resources/chromedriver.exe"); 
+	     WebDriver driver = new ChromeDriver(); 
+	     driver.get("http://134.33.193.148/");
+	     driver.manage().window().maximize();
+	     Thread.sleep(2000);
+	     String title = driver.getTitle();
+	     System.out.println("Page Title: " + title );
+	     String url = driver.getCurrentUrl();
+		 System.out.println("Page url is : " + url);
+	     driver.findElement(By.linkText("Login")).click();
+		 driver.findElement(By.id("formBasicEmail")).sendKeys("aparnamol");
+		 driver.findElement(By.id("formBasicPassword")).sendKeys("aparna123");
+		 driver.findElement(By.xpath("//a[@class='sc-bdvvtL cJEFiR' and contains(text(), 'Login ')]"));
+		 System.out.println("Logined  successfully");
+		 driver.navigate().to("http://134.33.193.148/dashboard");
+		 String T = driver.getTitle();
+	     System.out.println("After login Page Title is : " + T );
+	     String U = driver.getCurrentUrl();
+		 System.out.println("Page url is :" + U);
+		 
+		 
+//		 driver.close();
+	}
+
+}
