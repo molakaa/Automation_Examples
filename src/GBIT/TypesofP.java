7e529cf8bd5fe2175872432de8ecbfbb42cd4af6
molakaa <molaka.saiaparna96@gmail.com>
2025-03-31 22:05:44 -0500

    pushing type of p

--- src/GBIT/TypesofP.java
+++ src/GBIT/TypesofP.java
@@ -18,23 +18,34 @@ public class TypesofP {
 	     driver.get("https://www.cbre.com");
 	     driver.manage().window().maximize();
 	     Thread.sleep(2000);
+	     
 	     driver.findElement(By.id("onetrust-accept-btn-handler")).click();
 	     String title = driver.getTitle();
 	     System.out.println("Page Title: " + title );
 	     String url = driver.getCurrentUrl();
 		 System.out.println("Page url" +url);
+		 
 		 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));	 
 		 WebElement servicesMenu = driver.findElement(By.className("cbre-c-globalNav__link"));
 		 Actions actions = new Actions(driver);
 	     actions.moveToElement(servicesMenu).build().perform();
 		 Thread.sleep(2000);
+		 
 		 WebElement AllPropertyTypes = wait.until(ExpectedConditions.elementToBeClickable(
 		 By.xpath("//a[@class='cbre-c-ctaLink' and @href='/services/property-types' and text()='See All Property Types']")));
          AllPropertyTypes.click();
+		 Thread.sleep(2000);
+         String T = driver.getTitle();
+	     System.out.println("Page Title: " + T );
+	     
          WebElement netLeaseLink = wait.until(ExpectedConditions.presenceOfElementLocated(
          By.xpath("//a[@href='/services/property-types/alternatives/net-lease' and text()='Net Lease']")));
          netLeaseLink.click();
-         driver.close();           
+		 Thread.sleep(2000);
+         String P = driver.getTitle();
+	     System.out.println("Page Title: " + P );
+         driver.close();  
+         
 	}
 
 }
